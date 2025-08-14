ace8c233b093be212401f1e30f56f91f083061f3
kasmbafumoja <kaskas2008910@gmail.com>
2025-08-14 13:29:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PROVIDE YOUR SESSION ID",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~eoA0VAzZ#B0LAkwnClTnLFncBySTFVBPnmtYOF_8NLN53c92HsAQ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -48,7 +48,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "528145550855",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "243891908709",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ᴘʀɪɴᴄᴇ",
 // add bot owner name
